15f28a80e532e17deef2e1a5731b3ca8f8dfcda6
KhaledAshrafAhmed <khalidashraf0033@gmail.com>
2024-02-18 03:16:08 +0200

    my assignment

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,22 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+
+  const id = req.params.id;
+for (let i = 0; i < employee.length; i++) {
+  if (employee[i].id === id) {
+    employee.splice(i, 1);
+    return;
+  }
+}
+
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  for (let i = 0; i < employee.length; i++) {
+    if (employee[i].id === req.body.id) {
+      return;}
+    }
+employee.push({ id: req.body.id, name: req.body.name });
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -19,6 +19,7 @@ function fetchEmployees() {
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        deleteButton.addEventListener('click', () => deleteEmployee(item.id)); // Adding event listener
         deleteCell.appendChild(deleteButton);
 
         row.appendChild(deleteCell)
@@ -32,21 +33,40 @@ function fetchEmployees() {
 // TODO
 // add event listener to submit button
 
+document.getElementById('employeeForm').addEventListener('submit', function(event) { createEmployee();});
+
 // TODO
-// add event listener to delete button
+
 
 // TODO
 function createEmployee (){
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  var EmpName = document.getElementById('name').value;
+  var EmpId = document.getElementById('id').value;
+  const EmpEntity = {name: EmpName,id:EmpId};
+ fetch('http://localhost:3000/api/v1/employee', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(EmpEntity)
+})
+fetchEmployees();
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee (id){
   // get id
   // send id to BE
   // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee/${id}`, {
+    method: 'DELETE',
+  })
+
+
+  fetchEmployees();
 }
 
-fetchEmployees()
+fetchEmployees() 
